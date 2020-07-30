3c7cb1e26dba47047b6420d2026deeb53c66d1a7
Drew Bragg <DRBragg@gmail.com>
2020-07-30 12:08:30 -0400

    Add unread metions counting

--- app/channels/unreads_channel.rb
+++ app/channels/unreads_channel.rb
@@ -1,8 +1,10 @@
 class UnreadsChannel < ApplicationCable::Channel
   def subscribed
     stop_all_streams
+
     @channel_user = current_user.channel_users.find_by(channel_id: params['id'])
-    @channel = @channel_user.channel
+    @channel      = @channel_user.channel
+
     stream_for @channel
   end
 
--- app/javascript/controllers/unreads_controller.js
+++ app/javascript/controllers/unreads_controller.js
@@ -2,7 +2,11 @@ import { Controller } from 'stimulus'
 import consumer from 'channels/consumer'
 
 export default class extends Controller {
+  static targets = ['mentions']
+
   connect() {
+    this.username = document.querySelector("meta[name='username']").getAttribute('content')
+
     this.subscription = consumer.subscriptions.create(
       { channel: 'UnreadsChannel', id: this.data.get('id') },
       {
@@ -23,5 +27,42 @@ export default class extends Controller {
 
   _received(data) {
     this.element.classList.add('font-weight-bold')
+
+    if (data.mentions && data.mentions.includes(this.username)) {
+      this.updateUnreadMentions()
+      this.notify(data.body)
+    }
+  }
+
+  updateUnreadMentions() {
+    if (this.hasMentionsTarget) {
+      let count = parseInt(this.mentionsTarget.textContent)
+      this.mentionsTarget.textContent = count + 1
+      this.mentionsTarget.removeAttribute('hidden')
+    }
+  }
+
+  notify(message) {
+    console.log('notify', message)
+    // Let's check if the browser supports notifications
+    if (!('Notification' in window)) {
+      console.error('This browser does not support desktop notification')
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === 'granted') {
+      // If it's okay let's create a notification
+      var notification = new Notification(message)
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          var notification = new Notification(message)
+        }
+      })
+    }
   }
 }
